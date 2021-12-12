b0d8c0442a3c78f90e2e95f7d22ba49d3078bf78
Akellazp <nkalichik@gmail.com>
2021-12-12 16:12:48 +0200

    1

--- answers/files/1.48/script.js
+++ answers/files/1.48/script.js
@@ -1,19 +1,77 @@
+let operatorValue = 'ADDITTION';
+let operator = '+';
+
 function onLeftOperandChange(value) {
   console.log('onLeftOperandChange', value)
+  leftOperandElement.value = value;
 }
 
 function onRightOperandChange(value) {
   console.log('onRightOperandChange', value)
+  rightOperandElement.value = value;
 }
 
 function onOperatorChange(value) {
   console.log('onOperatorChange', value)
+  operatorValue = value;
 }
 
 function onSubmit () {
   console.log('onSubmit')
+  switch (operatorValue) {
+    case 'ADDITION' :
+      value = leftOperandElement.value + rightOperandElement.value;
+      operator = '+';
+      break;
+    case 'SUBTRACTION' :
+      value = leftOperandElement.value - rightOperandElement.value;
+      operator = '-';
+      break;
+    case 'MULTIPLICATION' :
+      value = leftOperandElement.value * rightOperandElement.value;
+      operator = '*';
+      break;
+    case 'EXPONENTIATION' :
+      value = leftOperandElement.value ** rightOperandElement.value;
+      operator = '^';
+      break;
+    case 'DIVISION' :
+      value = leftOperandElement.value / rightOperandElement.value;
+      operator = '/';
+      break;
+    case 'MODULUS' :
+      value = leftOperandElement.value % rightOperandElement.value;
+      operator = '%';
+      break;
+    case 'ROOT' :
+      value = leftOperandElement.value ** (1 / rightOperandElement.value);
+      operator = '√';
+      break;
+  }
+  setResult(value);
+  setExpression(`${leftOperandElement.value}${operator}${rightOperandElement.value}`);
 }
 
 function onClear () {
   console.log('onClear')
+  setExpression('');
 }
+
+switch (operatorValue) {
+  case 'ADDITION' :
+  case 'SUBTRACTION' :
+  case 'MULTIPLICATION' :
+  case 'EXPONENTIATION' :
+  case 'DIVISION' :
+  case 'MODULUS' :
+  case 'ROOT' :
+}
+/*
++
+-
+*
+**
+/
+%
+**(1/)
+*/
