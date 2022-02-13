7846bf4c87e9dbd27ace0f7e3300233f2b82a8bf
Akellazp <nkalichik@gmail.com>
2022-02-13 16:08:32 +0200

    1

--- answers/files/1.3/script.js
+++ answers/files/1.3/script.js
@@ -1,3 +1,18 @@
-let admin, name = 'John';
-admin = name;
-console.log(admin);
+class Chameleon {
+    constructor({ newColor = "green" } = {}) {
+      this.newColor = newColor;
+    }
+    
+    static colorChange(newColor) {
+      this.newColor = newColor;
+    }
+    
+    getColor() {
+      return this.newColor;
+    }
+  }
+  
+  const freddie = new Chameleon();
+  Chameleon.colorChange("orange");
+  const color = freddie.getColor();
+  console.log(color);
